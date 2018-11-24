a3be9acfa2d2e4cd45277facc2aa0d3bdf3ac633
NMWFTMA <36353335+NMWFTMA@users.noreply.github.com>
2018-11-25 03:28:05 +0900

    ネイルサロン予約サイト　プロジェクト新規作成
    ・spring bootプロジェクト作成
    ・thymeleaf設定
    ・共通ヘッダ、フッダ、メニューバー作成

---
+++ src/main/java/com/project/nail/HomeController.java
@@ -0,0 +1,18 @@
+package com.project.nail;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+		@RequestMapping(value="/", method=RequestMethod.GET)
+		public String root() {
+			return "index";
+		}
+
+		@RequestMapping(value="/top", method=RequestMethod.GET)
+		public String index() {
+			return "index";
+		}
+}
---
+++ src/main/java/com/project/nail/NailSalonProjectApplication.java
@@ -0,0 +1,12 @@
+package com.project.nail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailSalonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailSalonProjectApplication.class, args);
+	}
+}
