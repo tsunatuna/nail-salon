4d0f4326a6bc9dafa75a8da2758f71c0e5b03a28
igaguri1616 <ukcnoy.1115.okashisuki@gmail.com>
2018-11-25 10:55:22 +0900

    ホーム画面、共通部品　js、cssファイル追加及び名前統一
    ・js、cssファイルを追加
    ・ホーム画面のファイル名がindex、home　→　topに統一

--- src/main/java/com/project/nail/TopController.java
+++ src/main/java/com/project/nail/TopController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HomeController {
+public class TopController {
 		@RequestMapping(value="/", method=RequestMethod.GET)
 		public String root() {
 			return "index";
