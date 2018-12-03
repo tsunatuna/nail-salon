7ff84852dc2a813cb2f427b28db901822b3d1f0a
igaguri1616 <igaguri1616@example.com>
2018-12-04 08:05:32 +0900

    全画面HTMLファイル作成　画面遷移確認
    Appricationクラスをパッケージに入れたら起動しなくなったため、元に戻した。

--- src/main/java/com/project/nail/NailSalonProjectApplication.java
+++ src/main/java/com/project/nail/NailSalonProjectApplication.java
@@ -1,4 +1,4 @@
-package com.project.nail.application;
+package com.project.nail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
